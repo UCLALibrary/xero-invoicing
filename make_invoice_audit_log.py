4c530f513565303c12750fa2e41fd46657f92ffc
Andy Kohler <akohler@library.ucla.edu>
2026-04-10 16:23:37 -0800

    Add retry capabilities and retrieval of all invoices

--- make_invoice_audit_log.py
+++ make_invoice_audit_log.py
@@ -3,14 +3,10 @@ import csv
 import datetime
 import requests
 import tomllib
-import webbrowser
-from oauthlib.oauth2 import WebApplicationClient
-from typing import TypeAlias
-from urllib.parse import parse_qsl, urlparse
-from pprint import pprint
+from retry.api import retry_call
+from xero_utils import config_dict, get_access_token, get_tenant_id
 
-# For convenience
-config_dict: TypeAlias = dict[str, str]
+# from pprint import pprint
 
 
 def _get_args() -> argparse.Namespace:
@@ -35,209 +31,99 @@ def _get_xero_config(config_file_name: str) -> config_dict:
     return config["xero"]
 
 
-def _get_auth_request_url(xero_config: config_dict) -> str:
-    oauth_client = WebApplicationClient(xero_config["client_id"])
-    auth_request_url = oauth_client.prepare_request_uri(
-        uri=xero_config["authorization_url"],
-        redirect_uri=xero_config["redirect_url"],
-        scope=[xero_config["scope"]],
-        state=xero_config["state"],
-    )
-    return auth_request_url
-
-
-def _get_auth_code(url: str) -> str:
-    url_query = urlparse(url).query
-    query_data = dict(parse_qsl(url_query))
-    return query_data["code"]
-
-
-def _get_refresh_token(xero_config: config_dict) -> str | None:
-    try:
-        with open(xero_config["refresh_token_file"], "r") as f:
-            refresh_token = f.read()
-    except FileNotFoundError:
-        refresh_token = None
-    return refresh_token
-
-
-def _store_refresh_token(xero_config: config_dict, refresh_token: str) -> None:
-    with open(xero_config["refresh_token_file"], "w") as f:
-        f.write(refresh_token)
-
-
-def _get_token_from_auth_code(auth_code: str, xero_config: config_dict) -> str:
-    # requests basic auth: auth=(client_id, client_secret)
-    # automatically does the same as this:
-    # b64_id_secret = b64encode(bytes(client_id + ":" + client_secret, "utf-8")).decode("utf-8")
-    # no need to include explicit Basic Authorization headers in request:
-    # headers={"Authorization": "Basic " + b64_id_secret},
-
-    response = requests.post(
-        xero_config["token_url"],
-        auth=(xero_config["client_id"], xero_config["client_secret"]),
-        data={
-            "grant_type": "authorization_code",
-            "code": auth_code,
-            "redirect_uri": xero_config["redirect_url"],
-        },
-    )
-    json_response = response.json()
-    _store_refresh_token(xero_config, json_response["refresh_token"])
-    return json_response["access_token"]
-
-
-def _get_token_from_refresh_token(refresh_token: str, xero_config: config_dict) -> str:
-    response = requests.post(
-        xero_config["token_url"],
-        auth=(xero_config["client_id"], xero_config["client_secret"]),
-        data={
-            "grant_type": "refresh_token",
-            "refresh_token": refresh_token,
-        },
-    )
-    json_response = response.json()
-    pprint(json_response, width=132)
-    _store_refresh_token(xero_config, json_response["refresh_token"])
-    return json_response["access_token"]
-
-
-def get_access_token(xero_config: config_dict) -> str:
-    # If we have a refresh token stored, use it to get a new access token.
-    # Otherwise, we have to use the web-based authorization flow to get
-    # an access code and exchange it for an access token.
-    refresh_token = _get_refresh_token(xero_config)
-    if refresh_token:
-        # use it
-        access_token = _get_token_from_refresh_token(refresh_token, xero_config)
-    else:
-        # Use the web-based authorization flow
-        auth_request_url = _get_auth_request_url(xero_config)
-        print(auth_request_url)
-        webbrowser.open_new(auth_request_url)
-
-        auth_response_url = input("What URL did Xero return? ")
-        auth_code = _get_auth_code(auth_response_url)
-        access_token = _get_token_from_auth_code(auth_code, xero_config)
-
-    return access_token
-
-
-def get_tenant_id(access_token: str, xero_config: config_dict) -> str:
-    response = requests.get(
-        xero_config["tenant_url"],
-        headers={
-            "Authorization": "Bearer " + access_token,
-            "Content-Type": "application/json",
-        },
-    )
-    json_response = response.json()
-    # Response is a list of dicts, one for each organization associated with our credentials.
-    # We should have just one... probably could hard-code it, but for now, just return the
-    # first "ORGANISATION" tenant id found.
-    tenant_ids = [
-        tenant["tenantId"]
-        for tenant in json_response
-        if tenant["tenantType"] == "ORGANISATION"
-    ]
-    return tenant_ids[0]
-
-
-# def _api_usage_sucks_i_hate_this_sdk():
-#     api_client = ApiClient(
-#         Configuration(
-#             debug=False,
-#             oauth2_token=OAuth2Token(
-#                 client_id=xero_config["client_id"],
-#                 client_secret=xero_config["client_secret"],
-#             ),
-#         ),
-#         pool_threads=1,
-#     )
-
-#     @api_client.oauth2_token_getter
-#     def obtain_xero_oauth2_token():
-#         return xero_config["access_token"]
-
-#     @api_client.oauth2_token_saver
-#     def store_xero_oauth2_token(token):
-#         xero_config["access_token"] = access_token
-
-#     api_client.set_oauth2_token(access_token)
-
-#     accounting = AccountingApi(api_client)
-#     invoices = accounting.get_invoices
-#     pprint(invoices)
-
-
-def get_invoices(access_token: str, tenant_id: str) -> dict:
+def get_invoices(access_token: str, tenant_id: str) -> list[dict]:
     get_url = "https://api.xero.com/api.xro/2.0/Invoices"
     # Just "AUTHORISED" (aka "Awaiting Payment") and "PAID" statuses;
-    # "SUBMITTED" (aka "Awaiting Approval") is not yet approved so not relevant here.
+    # "SUBMITTED" (aka "Awaiting Approval") invoices are not yet approved,
+    # so not relevant here.
+
     # "Where": "DateString>='2025-11-19'" doesn't work: 400 Bad Request;
     # use "If-Modified-Since" header (not parameter) instead.
     # First date of production invoices: Nov 6 2025
     start_date = datetime.datetime(2025, 11, 6)
-    parameters = {
-        "Statuses": "AUTHORISED,PAID",
-        "order": "InvoiceNumber",
+    header_params = {
+        "Authorization": "Bearer " + access_token,
+        "Xero-tenant-id": tenant_id,
+        "Accept": "application/json",
+        "If-Modified-Since": start_date.strftime("%a, %d %b %Y %H:%M:%S GMT"),
     }
-    response = requests.get(
-        get_url,
-        headers={
-            "Authorization": "Bearer " + access_token,
-            "Xero-tenant-id": tenant_id,
-            "Accept": "application/json",
-            "If-Modified-Since": start_date.strftime("%a, %d %b %Y %H:%M:%S GMT"),
-        },
-        params=parameters,
-    )
-    print(response.status_code, response.reason)
-    json_response = response.json()
-    invoices = json_response["Invoices"]
-    # pprint(invoices[20], width=132)
+
+    invoices: list[dict] = []
+    page = 1
+    page_size = 100  # default is 100, can be up to 1000.
+    # The only way to know how many invoices there are is to retrieve until
+    # no more are received.
+    more_invoices = True
+    while more_invoices:
+        query_params = {
+            "Statuses": "AUTHORISED,PAID",
+            "order": "InvoiceNumber",
+            "page": page,
+            "pageSize": page_size,
+        }
+
+        response = requests.get(
+            get_url,
+            headers=header_params,
+            params=query_params,
+        )
+
+        if response.status_code != 200:
+            print(response.status_code, response.reason)
+        json_response = response.json()
+        this_batch: list[dict] = json_response["Invoices"]
+        if len(this_batch) > 0:
+            invoices.extend(this_batch)
+            page += 1
+        else:
+            more_invoices = False
+
+    print(f"Retrieved {len(invoices)} invoices.")
     return invoices
 
 
-def get_invoice(access_token: str, tenant_id: str, invoice_number: str) -> dict:
-    # TODO: Merge with get_invoices, if this goes beyond POC.
-    get_url = f"https://api.xero.com/api.xro/2.0/Invoices/{invoice_number}"
-    response = requests.get(
-        get_url,
-        headers={
-            "Authorization": "Bearer " + access_token,
-            "Xero-tenant-id": tenant_id,
-            "Accept": "application/json",
-        },
-    )
-    invoice = response.json()
-    return invoice
+# def get_invoice(access_token: str, tenant_id: str, invoice_number: str) -> dict:
+#     # TODO: Merge with get_invoices, if this goes beyond POC.
+#     get_url = f"https://api.xero.com/api.xro/2.0/Invoices/{invoice_number}"
+#     response = requests.get(
+#         get_url,
+#         headers={
+#             "Authorization": "Bearer " + access_token,
+#             "Xero-tenant-id": tenant_id,
+#             "Accept": "application/json",
+#         },
+#     )
+#     invoice = response.json()
+#     return invoice
 
 
-def get_account(access_token: str, tenant_id: str, account_id: str) -> dict:
-    get_url = f"https://api.xero.com/api.xro/2.0/Accounts/{account_id}"
-    response = requests.get(
-        get_url,
-        headers={
-            "Authorization": "Bearer " + access_token,
-            "Xero-tenant-id": tenant_id,
-            "Accept": "application/json",
-        },
-    )
-    account = response.json()
-    return account
+# def get_account(access_token: str, tenant_id: str, account_id: str) -> dict:
+#     get_url = f"https://api.xero.com/api.xro/2.0/Accounts/{account_id}"
+#     response = requests.get(
+#         get_url,
+#         headers={
+#             "Authorization": "Bearer " + access_token,
+#             "Xero-tenant-id": tenant_id,
+#             "Accept": "application/json",
+#         },
+#     )
+#     account = response.json()
+#     return account
 
 
 def get_invoice_history(access_token: str, tenant_id: str, invoice_id: str) -> dict:
     get_url = f"https://api.xero.com/api.xro/2.0/Invoices/{invoice_id}/History"
-    response = requests.get(
-        get_url,
-        headers={
-            "Authorization": "Bearer " + access_token,
-            "Xero-tenant-id": tenant_id,
-            "Accept": "application/json",
-        },
-    )
+    headers = {
+        "Authorization": "Bearer " + access_token,
+        "Xero-tenant-id": tenant_id,
+        "Accept": "application/json",
+    }
+    response = requests.get(url=get_url, headers=headers)
+    print(f"{invoice_id=}, {response.status_code=}")
+    if response.status_code != 200:
+        print(
+            f"Current API Retry-After: {response.headers.get("Retry-After", "No retry specified")}"
+        )
     history = response.json()
     return history
 
@@ -246,11 +132,10 @@ def main() -> None:
     args = _get_args()
     xero_config = _get_xero_config(args.config_file)
     access_token = get_access_token(xero_config)
-    # # To make api_client happy, store access_token in config
-    # xero_config["access_token"] = access_token
     tenant_id = get_tenant_id(access_token, xero_config)
 
     invoices = get_invoices(access_token, tenant_id)
+
     invoice_audit_data = []
     for invoice in invoices:
         invoice_number = invoice.get("InvoiceNumber", "NO INVOICE NUMBER")
@@ -258,7 +143,16 @@ def main() -> None:
         status = invoice.get("Status", "NO STATUS")
         invoice_id = invoice.get("InvoiceID", "NO INVOICE ID")
 
-        history = get_invoice_history(access_token, tenant_id, invoice_id)
+        # Limited by Xero to 60 calls/minute. We could check Retry-After response header,
+        # but easier just to use `retry_call()` with a relatively long backoff.
+        history = retry_call(
+            get_invoice_history,
+            fargs=[access_token, tenant_id, invoice_id],
+            tries=10,
+            delay=1,
+            backoff=4,
+            max_delay=60,
+        )
         history_records = history.get("HistoryRecords", [])
         # pprint(history_records, width=132)
         user_created = ""
@@ -274,6 +168,10 @@ def main() -> None:
             elif event == "Paid":
                 user_paid = user
 
+        # Flag invoices created and approved by the same user.
+        audit_message = "SAME USER" if user_created == user_approved else ""
+
+        # Data for output to report.
         invoice_audit_data.append(
             {
                 "invoice_number": invoice_number,
@@ -282,6 +180,7 @@ def main() -> None:
                 "user_created": user_created,
                 "user_approved": user_approved,
                 "user_paid": user_paid,
+                "audit_message": audit_message,
             }
         )
 
